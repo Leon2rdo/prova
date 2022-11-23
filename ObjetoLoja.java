ac8d6296ea71539aeb4f8734b19111024397e192
Leon2rdo <111002947+Leon2rdo@users.noreply.github.com>
2022-11-23 14:51:41 -0300

    Create ObjetoLoja.java

---
+++ ObjetoLoja.java
@@ -0,0 +1,15 @@
+package Calçado;
+
+
+public class ObjetoLoja {
+    public static void main(String[] args){
+        Calçado jao;
+        jao = new Calçado();
+        jao.nome ("Shampoo");
+        jao.valor (5);
+        jao.quantidade (5);
+        jao.comprar (2);
+        jao.vender (1);
+        jao.aumento (10);
+}
+}
